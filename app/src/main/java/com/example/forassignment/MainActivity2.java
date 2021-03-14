602215afbee7bb726f6325b8bbb7079fe871d459
Adrian Cruz <17162@students.mrgs.school.nz>
2021-03-14 22:30:34 +1300

    not finished

---
+++ app/src/main/java/com/example/forassignment/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.example.forassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button go = (Button) findViewById(R.id.go);
+        go.setTransformationMethod(null);
+        Intent intent = new Intent();
+    }
+
+}
